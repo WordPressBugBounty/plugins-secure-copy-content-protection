d98bdbee32b4236ca9a5431995b06a44bbd81ae4
Sean Murphy <sean@iamseanmurphy.com>
2025-06-12 11:17:07 +0000

    4.6.5

--- secure-copy-content-protection/includes/class-secure-copy-content-protection-i18n.php
+++ secure-copy-content-protection/includes/class-secure-copy-content-protection-i18n.php
@@ -34,13 +34,31 @@ class Secure_Copy_Content_Protection_i18n {
 	 */
 	public function load_plugin_textdomain() {
 
-		load_plugin_textdomain(
-			'secure-copy-content-protection',
-			false,
-			dirname(dirname(plugin_basename(__FILE__))) . '/languages/'
-		);
+		if ( version_compare( get_bloginfo( 'version' ), '6.7', '>=' ) ) {
+            $plugin = 'secure-copy-content-protection';
+            $locale = get_locale();
+
+			if ( is_textdomain_loaded( $plugin ) ) {
+                unload_textdomain( $plugin );
+            }
+            $mofile = sprintf( '%s-%s.mo', $plugin, $locale );
+            //check the installation language path first.
+            $domain_path = path_join( WP_LANG_DIR, 'plugins' );
+            $loaded = load_textdomain( $plugin, path_join( $domain_path, $mofile ) );
+
+            if ( ! $loaded ) { //else, check the plugin language folder.
+                $domain_path = path_join( WP_PLUGIN_DIR, "{$plugin}/languages" );
+                load_textdomain( $plugin, path_join( $domain_path, $mofile ) );
+            }
+        } else {
+            load_plugin_textdomain(
+				'secure-copy-content-protection',
+				false,
+				dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+			);
+
+        }
 
 	}
-
 
 }
